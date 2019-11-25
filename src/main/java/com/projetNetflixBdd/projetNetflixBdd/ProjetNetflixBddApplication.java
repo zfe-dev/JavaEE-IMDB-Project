8d6db111d82f698fe7cf11237ea3999f0cf40e61
petitpezlo <she4pheuMuid>
2019-11-25 10:33:29 +0100

    bdd ajouter

---
+++ src/main/java/com/projetNetflixBdd/projetNetflixBdd/ProjetNetflixBddApplication.java
@@ -0,0 +1,13 @@
+package com.projetNetflixBdd.projetNetflixBdd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetNetflixBddApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetNetflixBddApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projetNetflixBdd/projetNetflixBdd/ProjetNetflixBddApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetNetflixBdd.projetNetflixBdd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetNetflixBddApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
